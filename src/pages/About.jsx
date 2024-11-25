721e707be1a15a8cbbb1bbf77c2bc97740e3cf61
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-11-25 10:24:55 +0100

    add: routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,22 @@
+export default function Home() {
+    return (
+        <>
+            <header>
+                <div className="logo">Logo</div>
+                <nav>
+                    <a href="/">Home</a>
+                    <a href="/books">Books</a>
+                    <a href="/about">About</a>
+                    <a href="/contacts">Contacts</a>
+                </nav>
+            </header>
+            <main>
+                <h1>About</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo facilis est tempore similique inventore sequi animi temporibus aut reiciendis possimus, aliquam sapiente neque harum voluptatibus quasi praesentium ratione perspiciatis repudiandae?</p>
+            </main>
+            <footer>
+                <p>Copyrights 2024</p>
+            </footer>
+        </>
+    )
+}
